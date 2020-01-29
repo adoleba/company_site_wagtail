bc7f5285886567f9c8a50d52dd0355b06fde1d1a
adoleba <ania.doleba@wp.pl>
2020-01-29 15:14:00 +0100

    new app main page

--- company_site_wagtail/settings.py
+++ company_site_wagtail/settings.py
@@ -37,6 +37,8 @@ WAGTAIL_SITE_NAME = 'company_site_wagtail'
 # Application definition
 
 INSTALLED_APPS = [
+    'main',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',

389eecd48f3d1e4820c58d3de8454974ce483bec
adoleba <ania.doleba@wp.pl>
2020-02-02 16:36:59 +0100

    config smtp EmailBackend

--- company_site_wagtail/settings.py
+++ company_site_wagtail/settings.py
@@ -155,4 +155,10 @@ MEDIA_URL = '/media/'
 
 SITE_ID = 1
 
-EMAIL_BACKEND = 'django.core.mail.backends.dummy.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+EMAIL_HOST = env('EMAIL_HOST')
+EMAIL_HOST_USER = env('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
+EMAIL_PORT = env('EMAIL_PORT')
+EMAIL_USE_TLS = env('EMAIL_USE_TLS')

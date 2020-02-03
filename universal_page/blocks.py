83a94afafaa9c48ea02797cf22ee62761856589a
adoleba <ania.doleba@wp.pl>
2020-02-03 17:28:50 +0100

    blocks and model for universal_page, init templates

---
+++ universal_page/blocks.py
@@ -0,0 +1,38 @@
+from wagtail.core import blocks
+
+
+class WhiteHeaderWithButton(blocks.StreamBlock):
+    header_text = blocks.CharBlock(classname="header text")
+    button_text = blocks.CharBlock(classname="header text")
+    button_url = blocks.PageChooserBlock(classname="internal url")
+
+    class Meta:
+        template = 'universal_page/blocks/white_header_with_button.html'
+
+
+class ThreeColumnsWithHeaders(blocks.StreamBlock):
+    column_no_1_header = blocks.CharBlock(classname="header text column no 1")
+    column_no_1_text = blocks.RichTextBlock(classname="text column no 1")
+    column_no_2_header = blocks.CharBlock(classname="header text column no 2")
+    column_no_2_text = blocks.RichTextBlock(classname="text column no 2")
+    column_no_3_header = blocks.CharBlock(classname="header text column no 3")
+    column_no_3_text = blocks.RichTextBlock(classname="text column no 3")
+
+    class Meta:
+        template = 'universal_page/blocks/three_columns_with_headers.html'
+
+
+class Faq(blocks.StreamBlock):
+    question = blocks.CharBlock(classname="question")
+    answer = blocks.RichTextBlock(classname="answer")
+
+    class Meta:
+        template = 'universal_page/blocks/faq.html'
+
+
+class BlockQuoteWithHeader(blocks.StreamBlock):
+    header_text = blocks.RichTextBlock(classname="header text")
+    paragraph_text = blocks.RichTextBlock(classname="paragraph text")
+
+    class Meta:
+        template = 'universal_page/blocks/blockquote_with_header.html'
--- universal_page/models.py
+++ universal_page/models.py
@@ -1,3 +1,25 @@
-from django.db import models
+from wagtail.admin.edit_handlers import StreamFieldPanel
+from wagtail.core.fields import StreamField
+from wagtail.core.models import Page
+from wagtail.images.blocks import ImageChooserBlock
 
-# Create your models here.
+from universal_page.blocks import WhiteHeaderWithButton, ThreeColumnsWithHeaders, Faq, BlockQuoteWithHeader
+from wagtail.core import blocks
+
+
+class UniversalPage(Page):
+    body = StreamField([
+        ('grey_header', blocks.CharBlock(classname="grey_header")),
+        ('white_header_with_button', WhiteHeaderWithButton()),
+        ('three_columns_with_headers', ThreeColumnsWithHeaders()),
+        ('small_grey_header', blocks.CharBlock(classname="small_grey_header")),
+        ('faq', Faq()),
+        ('blockquote', blocks.CharBlock(classname="blockquote")),
+        ('blockquote_with_header', BlockQuoteWithHeader()),
+        ('paragraph', blocks.RichTextBlock()),
+        ('image', ImageChooserBlock(icon="image")),
+    ], null=True, blank=True)
+
+    content_panels = Page.content_panels + [
+        StreamFieldPanel('body')
+    ]

0e4b4d044d2db5bd42e79529bf0dcf66b15c9cd9
adoleba <ania.doleba@wp.pl>
2020-02-04 15:52:52 +0100

    Rebuilt HomePage models

--- home/models.py
+++ home/models.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 
 from django.db import models
-from wagtail.admin.edit_handlers import FieldPanel
+from wagtail.admin.edit_handlers import FieldPanel, PageChooserPanel
 
 from wagtail.core.models import Page
 
@@ -13,10 +13,19 @@ class HomePage(Page):
     about_description = models.TextField()
     first_card_title = models.CharField(max_length=200)
     first_card_description = models.TextField()
+    first_card_button_text = models.CharField(max_length=20)
+    first_card_button_url = models.ForeignKey('wagtailcore.Page', on_delete=models.SET_NULL,
+                                              null=True, related_name='+')
     second_card_title = models.CharField(max_length=200)
     second_card_description = models.TextField()
+    second_card_button_text = models.CharField(max_length=20)
+    second_card_button_url = models.ForeignKey('wagtailcore.Page', on_delete=models.SET_NULL,
+                                               null=True, related_name='+')
     third_card_title = models.CharField(max_length=200)
     third_card_description = models.TextField()
+    third_card_button_text = models.CharField(max_length=20)
+    third_card_button_url = models.ForeignKey('wagtailcore.Page', on_delete=models.SET_NULL,
+                                              null=True, related_name='+')
     edited = models.DateTimeField(auto_now=datetime.now)
 
     content_panels = Page.content_panels + [
@@ -26,8 +35,14 @@ class HomePage(Page):
         FieldPanel('about_description', classname="full"),
         FieldPanel('first_card_title', classname="full"),
         FieldPanel('first_card_description', classname="full"),
+        FieldPanel('first_card_button_text', classname="full"),
+        PageChooserPanel('first_card_button_url'),
         FieldPanel('second_card_title', classname="full"),
         FieldPanel('second_card_description', classname="full"),
+        FieldPanel('second_card_button_text', classname="full"),
+        PageChooserPanel('second_card_button_url'),
         FieldPanel('third_card_title', classname="full"),
         FieldPanel('third_card_description', classname="full"),
+        FieldPanel('third_card_button_text', classname="full"),
+        PageChooserPanel('third_card_button_url'),
     ]
